61752c3af3128409ed117253e08c24f0616ef92f
Larissa Midori <>
2021-05-26 02:52:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Operadores aritméticos
+
+n1 = int(input('Digite um nº: '))
+n2 = int(input('Digite outro nº: '))
+
+s = n1 + n2
+m = n1 * n2
+e = n1 ** n2
+d = n1 / n2
+di = n1 // n2
+mo = n1 % n2
+
+print(f'Soma = {s}, Multiplicação = {m}, Exponenciação = {e}, Divisão = {d:.2f}, Divisão Inteira = {di} e Módulo = {mo}')
